fe5eb0422e8cba03130ad432b7485cd2c95618bf
Shubhankar Kaushik <56501986+Punosie@users.noreply.github.com>
2024-12-02 01:05:45 +0530

    Updated Resume link

--- src/AboutMe/Pdf.js
+++ src/AboutMe/Pdf.js
@@ -1,5 +1,5 @@
 const onButtonClick=()=>{
-    window.open("https://drive.google.com/file/d/1vjQT29xPGN4GmU5foF8tvjvtjWOp0E60/view?usp=sharing", "_blank")
+    window.open("https://drive.google.com/file/d/1KFC9gj4Cw3L57eKBA_d6Vgkvth1pitT_/view?usp=sharing", "_blank")
 }
 
 export default onButtonClick;
