4f0529f633ef7988262e2805d84411b426fb97ef
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2025-04-23 07:57:42 +0530

    Update resume link

--- src/AboutMe/Pdf.js
+++ src/AboutMe/Pdf.js
@@ -1,5 +1,5 @@
 const onButtonClick=()=>{
-    window.open("https://drive.google.com/file/d/1KFC9gj4Cw3L57eKBA_d6Vgkvth1pitT_/view?usp=sharing", "_blank")
+    window.open("https://drive.google.com/file/d/18nD4u4oOdg1ioF5c8iZo1kPbvFlgyG2j/view?usp=sharing", "_blank")
 }
 
 export default onButtonClick;
